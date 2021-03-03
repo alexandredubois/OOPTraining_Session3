29bb0cf645dab49686e7eb3f43d04ae03d1c9179
Alexandre Dubois <contact@alexandredubois.com>
2021-03-03 15:19:05 +0100

    First commit

---
+++ VehiculeManager/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VehiculeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vous pouvez ajouter ici votre code à exécuter
+           
+
+
+            //N'enlevez pas la ligne suivante qui devra toujours être la dernière de votre programme :)
+            Vehicule.QuiEstDansLesParages();
+        }
+    }
+}
---
+++ VehiculeManager/Vehicule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace VehiculeManager
+{
+    public abstract class Vehicule
+    {
+        public string Couleur { get; set; }
+        public int Vitesse { get; set; }
+        private static List<Vehicule> Liste;
+
+        public Vehicule()
+        {
+            Couleur = "inconnue";
+            Vitesse = 0;
+            AjouterDansListe();
+        }
+
+        public void QuiSuisJe()
+        {
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "Je suis un véhicule non-identifié de couleur {0} qui se déplace à {1} km/h", Couleur, Vitesse));
+        }
+
+        public static void QuiEstDansLesParages()
+        {
+            Console.WriteLine("---------------------------------");
+            if (Liste != null && Liste.Count > 0)
+            {
+                Console.WriteLine("Il y a du monde ici !");
+                foreach (Vehicule item in Liste)
+                {
+                    item.QuiSuisJe();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Personne dans les parages...");
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+        private void AjouterDansListe()
+        {
+            if (Liste == null)
+                Liste = new List<Vehicule>();
+
+            Liste.Add(this);
+        }
+    }
+}
